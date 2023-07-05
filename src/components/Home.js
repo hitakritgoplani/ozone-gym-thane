85d6c3505ea12248baac2c0560d2b2f136658fab
Hitakrit Goplani <95306851+hitakritgoplani@users.noreply.github.com>
2023-07-05 22:36:58 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
 	<div className='home-root'>
 		<h1>Ozone - A Family Fitness Center</h1><br/><br/>
-	  	<h2>Welcome to Thane's <span className='largename'>LARGEST</span> & Most <span className='premium'>PREMIUM</span> Fitness Hub</h2>
+	  	<h2>Welcome to Thane's <span className='largename'>OLDEST</span> & Most <span className='premium'>PREMIUM</span> Fitness Hub</h2>
 	</div>
   )
 }
